427a4c1f608841076188b8ff1a6fd7085bb5438b
humam.f <humamfahurrahman@gmail.com>
2023-04-09 21:43:59 +0700

    add

---
+++ README.md
@@ -0,0 +1,10 @@
+## Profil
+
+|                 | Biodata            |
+| --------------- | ------------------ |
+| **Nama**        | Humam Fathurrahman |
+| **NIM**         | 312010389          |
+| **Kelas**       | TI.20.A2           |
+| **Mata Kuliah** | Pemrograman Mobile |
+
+![Hasil](SS.png)
---
+++ kelas.php
@@ -0,0 +1,64 @@
+<?php
+$url = 'https://api.steinhq.com/v1/storages/642110d4eced9b09e9c62384/20A2';
+$isi = file_get_contents($url);
+$data = json_decode($isi, true);
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <title>Absen Mahasiswa</title>
+</head>
+
+<body>
+<h2 class="text-center"><br><br>Absensi Mahasiswa TI.20.A2</h2><br><br>
+    <div class="container">
+        <table id="example" class="table table-bordered">
+            <thead>
+                <tr>
+                    <th class="text-center">No</th>
+                    <th class="text-center">Nama</th>
+                    <th class="text-center">NIM</th>
+                    <th class="text-center">1</th>
+                    <th class="text-center">2</th>
+                    <th class="text-center">3</th>
+                    <th class="text-center">4</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <?php 
+                    foreach ($data as $item) {
+                    ?>            
+                        <td><?php echo $item['NO']; ?></td>
+                        <td><?php echo $item['Nama']; ?></td>
+                        <td><?php echo $item['NIM']; ?></td>
+                        <td><?php echo $item['1']; ?></td>
+                        <td><?php echo $item['2']; ?></td>
+                        <td><?php echo $item['3']; ?></td>
+                        <td><?php echo $item['4']; ?></td>
+                </tr><?php } ?>
+            </tbody>
+        </table>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap4.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#example').DataTable();
+        });
+    </script>
+</body>
+
+</html>
